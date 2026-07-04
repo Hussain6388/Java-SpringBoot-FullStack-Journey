41a0bc59bc125a8002c85e8de84f4c93b90d37df
Hussain6388 <surfrajhussanambi8268@gmail.com>
2026-07-04 10:38:10 +0530

    Add files via upload

---
+++ Lecture 07/BeanInitializationDemo/src/main/java/in/strinke/AppConfig.java
@@ -0,0 +1,9 @@
+package in.strinke;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
---
+++ Lecture 07/BeanInitializationDemo/src/main/java/in/strinke/PaymentService.java
@@ -0,0 +1,17 @@
+package in.strinke;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class PaymentService {
+
+    public PaymentService() {
+        System.out.println("PaymentService Created");
+    }
+
+    public void pay(){
+        System.out.println("Payment Successful");
+    }
+}
